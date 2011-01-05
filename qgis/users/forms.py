9fec9ea65cacb25f8f8fe737b4e3d877c6a6ca98
Sam <sam@sam-desktop.(none)>
2011-01-05 22:21:26 +0200

    added olwidget to MEDIA_ROOT and install requirements.txt

--- qgis/users/forms.py
+++ qgis/users/forms.py
@@ -1,8 +1,24 @@
 from users.models import *
 from django import forms
+from django.forms import ModelForm, Textarea, TextInput
+
 
 class QgisUserForm(forms.ModelForm):
+  name = forms.CharField(label="Name:",
+               required=True,
+               widget=forms.TextInput(attrs={'size': 50,}),
+               help_text="",
+               error_messages={'required': 
+               '''Please enter the name of the user'''},
+              )
+
   class Meta:
     model = QgisUser
+    exclude = ('added_date', 'guid',)
+    widgets = {
+            'email': TextInput(attrs={'size': 50,}),
+            'home_url': TextInput(attrs={'size': 50,}),                        
+        }
+
 
 
