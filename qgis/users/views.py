5c4cad4f80d4fc65959d456bb897a31b2b295218
Tim Sutton <tim@linfiniti.com>
2011-01-05 11:57:05 +0200

    Fix css display issues due to request context var not being passed to tempaltes

--- qgis/users/views.py
+++ qgis/users/views.py
@@ -2,9 +2,10 @@ from django.http import HttpResponse
 from users.models import *
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.template import RequestContext
   
 def usersMap(theRequest):
   myObjects = QgisUser.objects.all()
-  return render_to_response("view_users.html", {'myObjects' : myObjects})
+  return render_to_response("view_users.html", {'myObjects' : myObjects}, context_instance=RequestContext(theRequest))
 
 
