733904fcf47622896f5b18574d647d9825b3d38e
Sam <sam@sam-desktop.(none)>
2011-01-06 00:32:12 +0200

    added olwidget to form (currently now working - to be resolved)

--- qgis/users/forms.py
+++ qgis/users/forms.py
@@ -1,24 +1,46 @@
 from users.models import *
 from django import forms
 from django.forms import ModelForm, Textarea, TextInput
+from olwidget.widgets import EditableMap
+from olwidget.forms import MapModelForm
 
-
-class QgisUserForm(forms.ModelForm):
-  name = forms.CharField(label="Name:",
+class QgisUserForm(MapModelForm):
+  name = forms.CharField(label="User name:",
                required=True,
                widget=forms.TextInput(attrs={'size': 50,}),
                help_text="",
                error_messages={'required': 
-               '''Please enter the name of the user'''},
+               '''Please enter the QGIS user's name'''},
               )
+  email = forms.EmailField(label="Email:",
+               required=True,
+               widget=forms.TextInput(attrs={'size': 50,}),
+               help_text="This will not be displayed on the map",
+               error_messages={'required': 
+               '''Please enter the email address you wish to use'''},
+              )
+  home_url = forms.URLField(label="User home page:",
+               required=False,
+               widget=forms.TextInput(attrs={'size': 50,}),
+               help_text="",
+             )    
+  image = forms.ImageField(label="User profile picture:",
+               required=False,
+               help_text="",
+             )
+  geometry = forms.CharField(label="User location:",
+               required=True,
+               widget=EditableMap(),
+               help_text="Please enter your base location",
+               error_messages={'required': 
+               '''A location is required'''},
+              )                           
+                                     
 
   class Meta:
     model = QgisUser
     exclude = ('added_date', 'guid',)
-    widgets = {
-            'email': TextInput(attrs={'size': 50,}),
-            'home_url': TextInput(attrs={'size': 50,}),                        
-        }
+   
 
 
 
