fa3f986c974ee0f7dc13eaee0b35125f820e7293
Tim Sutton <tim@linfiniti.com>
2011-01-09 22:09:05 +0200

    Fixes for user email reminders

--- qgis/users/views.py
+++ qgis/users/views.py
@@ -10,6 +10,8 @@ from olwidget.widgets import Map, EditableLayer, InfoLayer, InfoMap
 from django.core.mail import send_mail
 
 from users.models import *
+# python logging support to django logging middleware
+import logging
   
 def usersMap(theRequest):
   myObjects = QgisUser.objects.all()
@@ -46,20 +48,25 @@ def updateUser(theRequest, theId):
 def emailEditAddress(theRequest):
   
   if theRequest.method == 'POST':
-    myForm = EmailForm(theRequest.POST)
-    recipient = myForm['email']
+    logging.info (" Form was posted!")
+    myForm = EmailForm( theRequest.POST )
+    logging.info("Email received from form was: %s" % myForm.data['email'])
+    recipient = myForm.data['email']
     myUser = get_object_or_None(QgisUser, email=recipient)
+    logging.info (" User is: %s" % myUser)
     if myForm.is_valid() and myUser:
+      logging.info(" User if valid and form is valid")
       myLink = "http://users.qgis.org/community-map/edit/" + myUser.guid 
       subject = "QGIS Community Map: Edit Link Reminder"
-      message = """Someone, hopefully you, has asked for a reminder for the unique link that will allow you to edit your profile settings on our QGIS user's map. To edit your location on our QGIS community map, please follow this link <a href="%s">$s</a>.""" % (myLink,myLink)
+      message = """Someone, hopefully you, has asked for a reminder for the unique link that will allow you to edit your profile settings on our QGIS user's map. To edit your location on our QGIS community map, please follow this link <a href="%s">%s</a>.""" % (myLink,myLink)
       message += "" 
       sender = "QGIS community website"
          
-      send_mail(subject, message, sender, recipient)
+      send_mail(subject, message, sender, [recipient])
         
       return HttpResponseRedirect("/community-map/edit/email_confirm.html")
     else:
+      logging.info(" User or form is NOT valid")
       return render_to_response("update_user.html", {'myForm' : myForm}, context_instance=RequestContext(theRequest))
 
   else:
