c513fefef85ba23cbd36c34da1448717b1e6fbff
Gary Sherman <gsherman@geoapt.com>
2011-01-26 17:55:58 -0800

    Shell script to update the feeds. Can be run from the prompt or via cron. SITE_DIR must be set to the location of the site directory

---
+++ update_planet.sh
@@ -0,0 +1,9 @@
+# Run feedjack_update.py to update the planet postings
+SITE_DIR=/home/web/qgis-django
+source $SITE_DIR/python/bin/activate
+# set up the environment
+export PYTHONPATH=$SITE_DIR:$SITE_DIR/qgis
+export DJANGO_SETTINGS_MODULE=qgis.settings
+# update the feeds
+feedjack_update.py
+
