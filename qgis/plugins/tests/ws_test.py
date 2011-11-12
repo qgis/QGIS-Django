d66022bfb1a40db20c2b5e160d15026ac32d5d88
Alessandro Pasotti <apasotti@gmail.com>
2011-11-12 13:13:07 +0100

    Initial WS test

---
+++ qgis/plugins/tests/ws_test.py
@@ -0,0 +1,22 @@
+"""
+
+Test routing services
+
+
+
+>>> from django.test.client import Client
+>>> c = Client()
+>>> from xmlrpclib import *
+>>> from django.core.management import call_command
+
+
+Utility function:
+
+def xmldecode(r):p, u = getparser();p.feed(r.content);p.close();return u._stack[0]
+
+>>> def xmldecode(r):
+...     p, u = getparser()
+...     p.feed(r.content)
+...     p.close()
+...     return u._stack[0]
+
