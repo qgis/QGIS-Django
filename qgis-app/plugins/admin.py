eb8ea127cdc5a8114eac9db7436f27d5518624b8
Alessandro Pasotti <apasotti@gmail.com>
2012-10-07 11:58:43 +0200

    Added name search in plugin admin

--- qgis-app/plugins/admin.py
+++ qgis-app/plugins/admin.py
@@ -5,6 +5,7 @@ from django.contrib.gis import admin
 class PluginAdmin (admin.ModelAdmin):
     list_filter     = ('featured',)
     list_display    = ('name', 'featured', 'created_by', 'created_on', 'downloads', 'stable', 'experimental')
+    search_fields   = ('name',)
 
 
 class PluginVersionAdmin (admin.ModelAdmin):
