192b47a67754b3bace1ca754e87803edae689506
Alessandro Pasotti <apasotti@gmail.com>
2012-10-19 15:35:35 +0200

    Fix transaction rollback error

--- qgis-app/plugins/views.py
+++ qgis-app/plugins/views.py
@@ -21,6 +21,7 @@ from plugins.forms import *
 from django.views.generic.list_detail import object_list, object_detail
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_protect
+from django.db import transaction
 
 from django.core.mail import send_mail
 from django.contrib.sites.models import Site
