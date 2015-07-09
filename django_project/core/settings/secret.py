6b789d2157a40ac16d8d20e16554aa3faf975a01
Akbar Gumbira <akbargumbira@gmail.com>
2015-07-09 12:31:12 +0800

    Add new settings structure.

---
+++ django_project/core/settings/secret.py
@@ -0,0 +1,3 @@
+# coding=utf-8
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'y2vu=4qarl)p=g_blq_c4afk!p6u_cor1gy1k@05ro=+tf7+)g'
---
+++ django_project/core/settings/utils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import os
+
+# Absolute filesystem path to the Django project directory:
+DJANGO_ROOT = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__))
+    )
+)
+
+def ABS_PATH(*args):
+    return os.path.join(DJANGO_ROOT, *args)
