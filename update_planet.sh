f8de9ec2545641f5fb5b2fb6e094f70d1d9bf7a8
Richard Duivenvoorde <richard@duif.net>
2016-07-19 16:38:21 +0200

    Revert "not needed? Writing an error in err.log"
    This reverts commit eb70f5e9fb1a7d33893f9159a07a6c5751f7dc7a.

--- update_planet.sh
+++ update_planet.sh
@@ -1,8 +1,7 @@
 cd /home/plugins/QGIS-Django/qgis-app
 # Run feedjack_update.py to update the planet postings
 SITE_DIR=/home/plugins/QGIS-Django/qgis-app
-# no venv here?
-#source /home/plugins/.virtualenvs/plugins/bin/activate
+source /home/plugins/.virtualenvs/plugins/bin/activate
 # set up the environment
 export PYTHONPATH=$SITE_DIR:$SITE_DIR
 export DJANGO_SETTINGS_MODULE=settings
