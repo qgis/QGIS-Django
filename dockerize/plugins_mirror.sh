b07b7f3357f06bdcebfce343d7c77917986d666a
Tim Sutton <tim@kartoza.com>
2020-07-10 11:36:19 +0100

    Update plugins_mirror.sh

--- dockerize/plugins_mirror.sh
+++ dockerize/plugins_mirror.sh
@@ -1,27 +1,28 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                                                              
 
-SOURCE=/home/backups/backups
-DEST=$1
+#First the database backups on the remote server
+SOURCE=/mnt/HC_Volume_4113275
+DEST=backups
 OBJECTIVE=$2
 REMOVE_FILE=$3
 
 shopt -s nocasematch
 if [ -z "$OBJECTIVE" ]
 then
-	OBJECTIVE='DOWNLOAD'
+    OBJECTIVE='DOWNLOAD'
 elif [[ $OBJECTIVE == 'upload' ]]; then
-	#statements
-	OBJECTIVE='UPLOAD'
-	TEMP=$SOURCE
-	SOURCE=$DEST
-	DEST=$TEMP
+    #statements
+    OBJECTIVE='UPLOAD'
+    TEMP=$SOURCE
+    SOURCE=$DEST
+    DEST=$TEMP
 else
-	OBJECTIVE='DOWNLOAD'
+    OBJECTIVE='DOWNLOAD'
 fi
 if [[ $REMOVE_FILE == 'True' ]]; then
-	REMOVE_FILE='-e'
+    REMOVE_FILE='-e'
 else
-	REMOVE_FILE=''
+    REMOVE_FILE=''
 fi
 shopt -u nocasematch
 
@@ -30,3 +31,36 @@ echo 'Destination :' $DEST
 echo 'Objective :' $OBJECTIVE
 
 lftp -u kartoza, sftp://plugins.qgis.org -e "mirror $REMOVE_FILE $SOURCE $DEST; bye"
+
+
+#Next the plugin backups on the remote server
+SOURCE=/mnt/HC_Volume_4113256/static
+DEST=static
+OBJECTIVE=$2
+REMOVE_FILE=$3
+
+shopt -s nocasematch
+if [ -z "$OBJECTIVE" ]
+then
+    OBJECTIVE='DOWNLOAD'
+elif [[ $OBJECTIVE == 'upload' ]]; then
+    #statements
+    OBJECTIVE='UPLOAD'
+    TEMP=$SOURCE
+    SOURCE=$DEST
+    DEST=$TEMP
+else
+    OBJECTIVE='DOWNLOAD'
+fi
+if [[ $REMOVE_FILE == 'True' ]]; then
+    REMOVE_FILE='-e'
+else
+    REMOVE_FILE=''
+fi
+shopt -u nocasematch
+
+echo 'Source :' $SOURCE
+echo 'Destination :' $DEST
+echo 'Objective :' $OBJECTIVE
+
+lftp -u kartoza, sftp://plugins.qgis.org -e "mirror $REMOVE_FILE $SOURCE $DEST; bye":
