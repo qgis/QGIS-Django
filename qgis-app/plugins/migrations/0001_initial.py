13c990b6d61e8668036013b99395cdf3b7dcacb9
dimasciput <dimas.ciputra@gmail.com>
2020-07-14 09:42:07 +0700

    Fix approve plugin does not work

--- qgis-app/plugins/migrations/0001_initial.py
+++ qgis-app/plugins/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('taggit', '0002_auto_20150616_2121'),
+        ('taggit', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
