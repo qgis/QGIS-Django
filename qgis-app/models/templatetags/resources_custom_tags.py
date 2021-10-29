ae6b4e41c47bff3eb319d6675f400f4e3003a906
Sumandari <sumandari@kartoza.com>
2021-10-29 08:59:49 +0800

    added version in template

--- qgis-app/models/templatetags/resources_custom_tags.py
+++ qgis-app/models/templatetags/resources_custom_tags.py
@@ -1,6 +1,9 @@
+import os.path
+
 import markdown
 
 from django import template
+from django.conf import settings
 from django.forms import CheckboxInput
 from django.utils.safestring import mark_safe
 
@@ -57,3 +60,17 @@ def md_to_html(md_string):
     md = markdown.Markdown()
     html = md.convert(md_string)
     return mark_safe(html)
+
+
+# inspired by projecta <https://github.com/kartoza/prj.app>
+@register.simple_tag(takes_context=True)
+def version_tag(context):
+    """Reads current project release from the .version file."""
+    version_file = os.path.join(settings.SITE_ROOT, '.version')
+    try:
+        with open(version_file, 'r') as file:
+            version = file.read()
+            context['version'] = version
+    except IOError:
+        context['version'] = 'Unknown'
+    return context['version']
