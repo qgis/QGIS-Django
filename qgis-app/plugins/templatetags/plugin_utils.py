f851253b6f3686f6b2bf71d3139305eb4e129d8d
Dimas Ciputra <dimas.ciputra@gmail.com>
2022-03-25 15:26:29 +0700

    Add plugin name to html title (#244)

--- qgis-app/plugins/templatetags/plugin_utils.py
+++ qgis-app/plugins/templatetags/plugin_utils.py
@@ -4,3 +4,17 @@ register = template.Library()
 @register.filter('klass')
 def klass(ob):
     return ob.__class__.__name__
+
+
+@register.simple_tag(takes_context=True)
+def plugin_title(context):
+    """Returns plugin name for title"""
+    title = ''
+    if 'plugin' in context:
+        title = context['plugin'].name
+    if 'version' in context:
+        title = '{plugin} {version}'.format(
+            plugin=context['version'].plugin.name,
+            version=context['version'].version
+        )
+    return title
