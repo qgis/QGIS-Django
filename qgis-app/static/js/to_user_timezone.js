561dc8cb03e0c888ff4e127798c2e43e2172bbd3
Sumandari <sumandari@kartoza.com>
2022-08-19 13:00:31 +0200

    added js to replace datetime to local tz

---
+++ qgis-app/static/js/to_user_timezone.js
@@ -0,0 +1,20 @@
+// Replace the date with local timezone
+
+$(".user-timezone").each(function (i) {
+  let localDate = toUserTimeZone($(this).text());
+  $(this).text(localDate);
+})
+
+function toUserTimeZone(date) {
+  try {
+    date = new Date(date);
+    let options = {
+      year: 'numeric', month: 'short', day: 'numeric',
+      hour: '2-digit', minute: '2-digit'
+    }
+    return date.toLocaleDateString([], options);
+  } catch (e) {
+    return date;
+  }
+}
+
