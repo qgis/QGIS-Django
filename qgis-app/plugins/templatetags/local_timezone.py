efb26d07776b8ec8c99af5d857e63ee08e1007ba
Lova ANDRIARIMALALA <43842786+Xpirix@users.noreply.github.com>
2023-11-28 16:04:21 +0300

    Implement moment.js, fix different displayed date

--- qgis-app/plugins/templatetags/local_timezone.py
+++ qgis-app/plugins/templatetags/local_timezone.py
@@ -6,10 +6,13 @@ register = template.Library()
 
 
 @register.filter(name="local_timezone", is_safe=True)
-def local_timezone(date):
+def local_timezone(date, args="LONG"):
     try:
         utcdate = date.astimezone(pytz.utc).isoformat()
-        result = '<span class="user-timezone">%s</span>' % (utcdate,)
+        if args and str(args).lower() == "short":
+            result = '<span class="short-user-timezone">%s</span>' % (utcdate,)
+        else:
+            result = '<span class="user-timezone">%s</span>' % (utcdate,)
     except AttributeError:
         result = date
     return mark_safe(result)
--- qgis-app/static/js/local_timezone.js
+++ qgis-app/static/js/local_timezone.js
@@ -5,13 +5,27 @@ $(".user-timezone").each(function (i) {
   $(this).text(localDate);
 })
 
-function toUserTimeZone(date) {
+$(".short-user-timezone").each(function (i) {
+  let localDate = toUserTimeZone($(this).text(), false);
+  $(this).text(localDate);
+})
+
+function toUserTimeZone(date, withTime=true) {
   try {
     date = new Date(date);
     let options = {
-      year: 'numeric', month: 'short', day: 'numeric',
-      hour: '2-digit', minute: '2-digit',
-      timeZoneName: 'short'
+      year: 'numeric', month: 'short', day: 'numeric'
+    }
+    if (withTime) {
+      options['hour'] = '2-digit'
+      options['minute'] = '2-digit'
+      options['timeZoneName'] = 'short'
+    }
+    const diffInDays = moment().diff(moment(date), 'days');
+
+    if (diffInDays <= 1 && !withTime) {
+      const distance = moment(date).fromNow();
+      return distance
     }
     return date.toLocaleDateString([], options);
   } catch (e) {
