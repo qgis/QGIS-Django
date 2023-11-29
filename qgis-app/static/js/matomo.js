ed53712f145a244bd0b3998a00fb71989ccacfef
Alexander CherryTea <Akiyamka@gmail.com>
2023-11-29 16:50:09 +0100

    add matomo script and connect it with base template

---
+++ qgis-app/static/js/matomo.js
@@ -0,0 +1,11 @@
+var _paq = window._paq = window._paq || [];
+_paq.push(["disableCookies"]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="//matomo.qgis.org/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '1']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
