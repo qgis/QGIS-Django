6fe3124a7767ccafa4590d8a988771cbdbf539c6
Lova ANDRIARIMALALA <43842786+Xpirix@users.noreply.github.com>
2023-12-23 14:41:40 +0300

    Fix different displayed date (#315)
    * Implement moment.js, fix different displayed date
    
    * Fix displayed date for Created on column

--- qgis-app/static/js/local_timezone.js
+++ qgis-app/static/js/local_timezone.js
@@ -5,13 +5,32 @@ $(".user-timezone").each(function (i) {
   $(this).text(localDate);
 })
 
-function toUserTimeZone(date) {
+$(".user-timezone-short").each(function (i) {
+  let localDate = toUserTimeZone($(this).text(), withTime=false);
+  $(this).text(localDate);
+})
+
+$(".user-timezone-short-naturalday").each(function (i) {
+  let localDate = toUserTimeZone($(this).text(), withTime=false, isNaturalDay=true);
+  $(this).text(localDate);
+})
+
+function toUserTimeZone(date, withTime=true, isNaturalDay=false) {
   try {
     date = new Date(date);
     let options = {
-      year: 'numeric', month: 'short', day: 'numeric',
-      hour: '2-digit', minute: '2-digit',
-      timeZoneName: 'short'
+      year: 'numeric', month: 'short', day: 'numeric'
+    }
+    if (withTime) {
+      options['hour'] = '2-digit'
+      options['minute'] = '2-digit'
+      options['timeZoneName'] = 'short'
+    }
+    const diffInDays = moment().diff(moment(date), 'days');
+
+    if (diffInDays <= 1 && isNaturalDay) {
+      const distance = moment(date).fromNow();
+      return distance
     }
     return date.toLocaleDateString([], options);
   } catch (e) {
