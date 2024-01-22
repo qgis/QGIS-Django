9d4045df282715b3f2fe76cf9b8a42a32fb4701d
Lova ANDRIARIMALALA <43842786+Xpirix@users.noreply.github.com>
2024-01-22 14:03:22 +0300

    Get static and media root from the environments

--- qgis-app/settings_docker.py
+++ qgis-app/settings_docker.py
@@ -11,7 +11,7 @@ ALLOWED_HOSTS = ["*"]
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = "/home/web/media/"
+MEDIA_ROOT = os.environ.get("MEDIA_ROOT", "/home/web/media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -24,7 +24,7 @@ MEDIA_URL = "/media/"
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = "/home/web/static"
+STATIC_ROOT = os.environ.get("STATIC_ROOT", "/home/web/static")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
