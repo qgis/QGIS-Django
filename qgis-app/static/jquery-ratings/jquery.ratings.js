936701aadb1084ffca75f07acb11a29914f00288
Lova ANDRIARIMALALA <43842786+Xpirix@users.noreply.github.com>
2024-02-13 14:21:09 +0300

    Add new stars images for plugins rating display

--- qgis-app/static/jquery-ratings/jquery.ratings.js
+++ qgis-app/static/jquery-ratings/jquery.ratings.js
@@ -42,8 +42,21 @@ jQuery.fn.ratings = function(stars, initialRating) {
       star.addClass('jquery-ratings-star');
       
       //Add the full css class if the star is beneath the initial rating.
+      // The last star will depend on the decimal left
       if(starIdx < initialRating) {
-        star.addClass('jquery-ratings-full');
+        let ratingDiff = initialRating - starIdx
+        if (ratingDiff > 0 && ratingDiff <= 0.3) {
+          star.addClass('jquery-ratings-quarter');
+        }
+        else if (ratingDiff > 0.3 && ratingDiff <= 0.6) {
+          star.addClass('jquery-ratings-half');
+        }
+        else if (ratingDiff > 0.6 && ratingDiff <= 0.9) {
+          star.addClass('jquery-ratings-half-and-quarter');
+        }
+        else {
+          star.addClass('jquery-ratings-full');
+        }
       }
       
       //add the star to the container
@@ -65,13 +78,28 @@ jQuery.fn.ratings = function(stars, initialRating) {
         //Unhighlight unselected stars.
         for(var index = this.rating; index < stars; index++) {
           starsCollection[index].removeClass('jquery-ratings-full');
+          starsCollection[index].removeClass('jquery-ratings-half-and-quarter');
+          starsCollection[index].removeClass('jquery-ratings-half');
+          starsCollection[index].removeClass('jquery-ratings-quarter');
         }
       });
       
       container.mouseleave(function() {
         //Highlight selected stars.
         for(var index = 0; index < containerElement.rating; index++) {
-          starsCollection[index].addClass('jquery-ratings-full');
+          let ratingDiff = containerElement.rating - index
+          if (ratingDiff > 0 && ratingDiff <= 0.3) {
+            starsCollection[index].addClass('jquery-ratings-quarter');
+          }
+          else if (ratingDiff > 0.3 && ratingDiff <= 0.6) {
+            starsCollection[index].addClass('jquery-ratings-half');
+          }
+          else if (ratingDiff > 0.6 && ratingDiff <= 0.9) {
+            starsCollection[index].addClass('jquery-ratings-half-and-quarter');
+          }
+          else {
+            starsCollection[index].addClass('jquery-ratings-full');
+          }
         }
         //Unhighlight unselected stars.
         for(var index = containerElement.rating; index < stars ; index++) {
