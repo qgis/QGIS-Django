ce76edc00ba18ea86b7bd715a7a9155efa82543e
Lova ANDRIARIMALALA <43842786+Xpirix@users.noreply.github.com>
2024-04-02 08:40:29 +0300

    Add nginx dev and prod configuration files

---
+++ dockerize/sites-enabled/docker-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Clean up sites-enabled
+echo "Clean sites-enabled"
+rm -rf /etc/nginx/conf.d/*.conf
+mkdir -p /etc/nginx/conf.d
+
+if [ $# -eq 1 ]; then
+	case $1 in
+		# Debug mode, enable dev.conf
+		[Dd][Ee][Bb][Uu][Gg])
+			echo "Run in debug mode"
+			CONF_FILE=dev.conf
+			ln -s /etc/nginx/sites-available/$CONF_FILE /etc/nginx/conf.d/$CONF_FILE
+			exec nginx -g "daemon off;"
+			;;
+		# Production mode, run using uwsgi
+		[Pp][Rr][Oo][Dd])
+			echo "Run in prod mode"
+			CONF_FILE=prod.conf
+			ln -s /etc/nginx/sites-available/$CONF_FILE /etc/nginx/conf.d/$CONF_FILE
+			exec nginx -g "daemon off;"
+			;;
+	esac
+fi
+
+# Run as bash entrypoint
+exec "$@"
