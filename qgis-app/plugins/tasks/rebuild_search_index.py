d095a5bdfec4b079c59d3681e988d67bd95acbea
Lova Andriarimalala <43842786+Xpirix@users.noreply.github.com>
2024-07-15 10:49:34 +0300

    Some improvements (#441)
    * Improve the rebuild search index task
    
    * Include dbbackups in make web command

--- qgis-app/plugins/tasks/rebuild_search_index.py
+++ qgis-app/plugins/tasks/rebuild_search_index.py
@@ -1,10 +1,12 @@
 from celery import shared_task
+from haystack.management.commands import update_index
 from celery.utils.log import get_task_logger
 
 logger = get_task_logger(__name__)
 
-
 @shared_task
-def rebuild_index():
-    import subprocess
-    subprocess.call(['python', 'manage.py', 'rebuild_index'])
+def rebuild_search_index():
+    """
+    Celery task to rebuild the search index.
+    """
+    update_index.Command().handle()
