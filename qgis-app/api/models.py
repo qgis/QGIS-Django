d48846343d72947928e38e2f13166efe2d5daf5c
Lova ANDRIARIMALALA <43842786+Xpirix@users.noreply.github.com>
2024-09-12 14:18:36 +0200

    Create user token model

--- qgis-app/api/models.py
+++ qgis-app/api/models.py
@@ -1,1 +1,32 @@
-# Create your models here.
+from base.models.processing_models import Resource
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
+from django.contrib.auth.models import User
+
+class HubOutstandingToken(models.Model):
+    """
+    Hub outstanding token
+    """
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    token = models.ForeignKey(
+        OutstandingToken,
+        on_delete=models.CASCADE
+    )
+    is_blacklisted = models.BooleanField(default=False)
+    is_newly_created = models.BooleanField(default=False)
+    description = models.CharField(
+        verbose_name=_("Description"),
+        help_text=_("Describe this token so that it's easier to remember where you're using it."),
+        max_length=512,
+        blank=True,
+        null=True,
+    )
+    last_used_on = models.DateTimeField(
+        verbose_name=_("Last used on"),
+        blank=True,
+        null=True
+    )
