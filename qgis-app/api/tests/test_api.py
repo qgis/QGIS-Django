99a2c88b4c9ca823d9175afea250535fcd62d3bc
Lova ANDRIARIMALALA <43842786+Xpirix@users.noreply.github.com>
2024-09-19 16:14:16 +0300

    Unit tests for get, update, delete API

--- qgis-app/api/tests/test_api.py
+++ qgis-app/api/tests/test_api.py
@@ -201,3 +201,209 @@ class TestResourceCreateView(SetUpTest, TestCase):
         response = self.client.post(url, data, format='multipart')
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.data, {"resource_type": "Resource type not supported"})
+
+
+@override_settings(MEDIA_ROOT=tempfile.mkdtemp())
+class TestResourceDetailView(SetUpTest, TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.token = RefreshToken.for_user(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token.access_token}')
+
+        uploaded_thumbnail = SimpleUploadedFile(
+            self.thumbnail_content.name, self.thumbnail_content.read()
+        )
+        uploaded_gpkg = SimpleUploadedFile(
+            self.gpkg_file_content.name, self.gpkg_file_content.read()
+        )
+        uploaded_qlr = SimpleUploadedFile(
+            self.qlr_file_content.name, self.qlr_file_content.read()
+        )
+        uploaded_model = SimpleUploadedFile(
+            self.modelzip_file_content.name, self.modelzip_file_content.read()
+        )
+        uploaded_style = SimpleUploadedFile(
+            self.stylexml_file_content.name, self.stylexml_file_content.read()
+        )
+        uploaded_wavefront = SimpleUploadedFile(
+            self.zip3dfile_content.name, self.zip3dfile_content.read()
+        )
+
+        self.geopackage = Geopackage.objects.create(
+            creator=self.user,
+            name="Test Geopackage",
+            description="A test geopackage",
+            thumbnail_image=uploaded_thumbnail,
+            file=uploaded_gpkg,
+        )
+        self.geopackage.approved = True
+        self.geopackage.save()
+
+        self.layerdefinition = LayerDefinition.objects.create(
+            creator=self.user,
+            name="Test Layer Definition",
+            description="A test layer definition",
+            thumbnail_image=uploaded_thumbnail,
+            file=uploaded_qlr,
+        )
+        self.layerdefinition.approved = True
+        self.layerdefinition.save()
+
+        self.model = Model.objects.create(
+            creator=self.user,
+            name="Test Model",
+            description="A test model",
+            thumbnail_image=uploaded_thumbnail,
+            file=uploaded_model,
+        )
+        self.model.approved = True
+        self.model.save()
+
+        self.style = Style.objects.create(
+            creator=self.user,
+            name="Test Style",
+            description="A test style",
+            thumbnail_image=uploaded_thumbnail,
+            file=uploaded_style,
+        )
+        self.style.approved = True
+        self.style.save()
+
+        self.wavefront = Wavefront.objects.create(
+            creator=self.user,
+            name="Test 3D Model",
+            description="A test 3D model",
+            thumbnail_image=uploaded_thumbnail,
+            file=uploaded_wavefront,
+        )
+        self.wavefront.approved = True
+        self.wavefront.save()
+
+
+    def test_get_geopackage(self):
+        url = reverse("resource-detail", kwargs={"uuid": self.geopackage.uuid, "resource_type": "geopackage"})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["name"], "Test Geopackage")
+
+    def test_get_layerdefinition(self):
+        url = reverse("resource-detail", kwargs={"uuid": self.layerdefinition.uuid, "resource_type": "layerdefinition"})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["name"], "Test Layer Definition")
+
+    def test_get_model(self):
+        url = reverse("resource-detail", kwargs={"uuid": self.model.uuid, "resource_type": "model"})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["name"], "Test Model")
+
+    def test_get_style(self):
+        url = reverse("resource-detail", kwargs={"uuid": self.style.uuid, "resource_type": "style"})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["name"], "Test Style")
+
+    def test_get_wavefront(self):
+        url = reverse("resource-detail", kwargs={"uuid": self.wavefront.uuid, "resource_type": "3dmodel"})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["name"], "Test 3D Model")
+
+    def test_update_geopackage(self):
+        url = reverse("resource-detail", kwargs={"uuid": self.geopackage.uuid, "resource_type": "geopackage"})
+        data = {
+            "name": "Updated Geopackage",
+            "description": "Updated description",
+            "thumbnail_full": self.geopackage.thumbnail_image,
+            "file": self.geopackage.file,
+        }
+        response = self.client.put(url, data, format="multipart")
+        self.assertEqual(response.status_code, 200)
+        self.geopackage.refresh_from_db()
+        self.assertEqual(self.geopackage.name, "Updated Geopackage")
+
+    def test_update_layerdefinition(self):
+        url = reverse("resource-detail", kwargs={"uuid": self.layerdefinition.uuid, "resource_type": "layerdefinition"})
+        data = {
+            "name": "Updated Layer Definition",
+            "description": "Updated description",
+            "thumbnail_full": self.layerdefinition.thumbnail_image,
+            "file": self.layerdefinition.file,
+        }
+        response = self.client.put(url, data, format="multipart")
+        self.assertEqual(response.status_code, 200)
+        self.layerdefinition.refresh_from_db()
+        self.assertEqual(self.layerdefinition.name, "Updated Layer Definition")
+
+    def test_update_model(self):
+        url = reverse("resource-detail", kwargs={"uuid": self.model.uuid, "resource_type": "model"})
+        data = {
+            "name": "Updated Model",
+            "description": "Updated description",
+            "thumbnail_full": self.model.thumbnail_image,
+            "file": self.model.file,
+        }
+        response = self.client.put(url, data, format="multipart")
+        self.assertEqual(response.status_code, 200)
+        self.model.refresh_from_db()
+        self.assertEqual(self.model.name, "Updated Model")
+
+    def test_update_style(self):
+        url = reverse("resource-detail", kwargs={"uuid": self.style.uuid, "resource_type": "style"})
+        data = {
+            "name": "Updated Style",
+            "description": "Updated description",
+            "thumbnail_full": self.style.thumbnail_image,
+            "file": self.style.file,
+        }
+        response = self.client.put(url, data, format="multipart")
+        self.assertEqual(response.status_code, 200)
+        self.style.refresh_from_db()
+        self.assertEqual(self.style.name, "Updated Style")
+
+    def test_update_wavefront(self):
+        url = reverse("resource-detail", kwargs={"uuid": self.wavefront.uuid, "resource_type": "3dmodel"})
+        data = {
+            "name": "Updated 3D Model",
+            "description": "Updated description",
+            "thumbnail_full": self.wavefront.thumbnail_image,
+            "file": self.wavefront.file,
+        }
+        response = self.client.put(url, data, format="multipart")
+        self.assertEqual(response.status_code, 200)
+        self.wavefront.refresh_from_db()
+        self.assertEqual(self.wavefront.name, "Updated 3D Model")
+
+    def test_delete_geopackage(self):
+        url = reverse("resource-detail", kwargs={"uuid": self.geopackage.uuid, "resource_type": "geopackage"})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 204)
+        self.assertFalse(Geopackage.objects.filter(uuid=self.geopackage.uuid).exists())
+
+    def test_delete_layerdefinition(self):
+        url = reverse("resource-detail", kwargs={"uuid": self.layerdefinition.uuid, "resource_type": "layerdefinition"})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 204)
+        self.assertFalse(LayerDefinition.objects.filter(uuid=self.layerdefinition.uuid).exists())
+
+    def test_delete_model(self):
+        url = reverse("resource-detail", kwargs={"uuid": self.model.uuid, "resource_type": "model"})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 204)
+        self.assertFalse(Model.objects.filter(uuid=self.model.uuid).exists())
+
+    def test_delete_style(self):
+        url = reverse("resource-detail", kwargs={"uuid": self.style.uuid, "resource_type": "style"})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 204)
+        self.assertFalse(Style.objects.filter(uuid=self.style.uuid).exists())
+
+    def test_delete_wavefront(self):
+        url = reverse("resource-detail", kwargs={"uuid": self.wavefront.uuid, "resource_type": "3dmodel"})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 204)
+        self.assertFalse(Wavefront.objects.filter(uuid=self.wavefront.uuid).exists())
